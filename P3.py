d6a64d6872a47f400827b55e322dc73faaac0a14
C-Dawn311 <courtneybingham311@gmail.com>
2026-03-25 16:29:24 -0600

    Created file
    I created a file and put the classes in there.

---
+++ P3.py
@@ -0,0 +1,34 @@
+# Authors: Courtney Bingham, Anna Pettit, Braden Adams, Ethan Lawson
+
+import random
+
+# Classes
+
+class Move:
+    def __init__(self, move_name, elemental_type, low_attack_points, high_attack_points):
+        self.move_name = move_name
+        self.elemental_type = elemental_type
+        self.low_attack_points = low_attack_points
+        self.high_attack_points = high_attack_points
+    # prints all info out
+    def get_info(self):
+        print(f"{self.move_name} (Type: {self.elemental_type}): {self.low_attack_points} to {self.high_attack_points} Attack Points")
+    # randomly generates number between low and high attack points
+    def generate_attack_value(self):
+        attack = random.randrange(self.low_attack_points,self.high_attack_points)
+        return attack
+
+
+class Pokemon:
+    def __init__(self, name, elemental_type, hit_points):
+        self.name = name
+        self.elemental_type = elemental_type
+        self.hit_points = self.hit_points
+    # prints out info
+    def get_info(self):
+        print(f"{self.name} - Type: {self.elemental_type} - Hit Points: {self.hit_points}")
+    # increases hit_poitns by 15 and prints out new messsage
+    def heal(self):
+        self.hit_points += 15
+        print(f"{self.name} has been healed to {self.hit_points} hit points.")
+
