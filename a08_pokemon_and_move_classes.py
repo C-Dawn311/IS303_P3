7825f4efab897fee77e3dba67600f4844b3b8386
akpettit <akpettit@byu.edu>
2026-03-25 18:54:44 -0600

    fixed move for loop
    corrected and functional for loop now

--- a08_pokemon_and_move_classes.py
+++ a08_pokemon_and_move_classes.py
@@ -47,8 +47,11 @@ Move9 = Move("Solar Beam", "Grass", 18, 27)
 lstMoves.append(Move1, Move2, Move3, Move4, Move5, Move6, Move7, Move8, Move9)
 
 for iCount in range(0,3): 
-    move = random.randrange(lstMoves)
-    move.get_info(lstMoves[move -1])
+    move_index = random.randrange(len(lstMoves))
+    move = lstMoves[move_index]
+
+    move.get_info()
     print("Generated attack value: ")
-    move.generate_attack_value(lstMoves[move -1])
-    lstMoves.remove(lstMoves[move - 1])
+    move.generate_attack_value()
+
+    lstMoves.pop(move_index)
