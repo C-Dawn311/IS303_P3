9b28e56217338067b99baff02c92b42e0c9fa1c3
EthanLawson101 <ethanlaw@byu.edu>
2026-03-26 11:44:51 -0600

    Ethan Lawson edit

--- a08_pokemon_and_move_classes.py
+++ a08_pokemon_and_move_classes.py
@@ -5,6 +5,7 @@ import random
 # Classes
 
 class Move:
+    # Constructor - takes move name, elemental type, and attack point range
     def __init__(self, move_name, elemental_type, low_attack_points, high_attack_points):
         self.move_name = move_name
         self.elemental_type = elemental_type
@@ -15,11 +16,12 @@ class Move:
         return(f"{self.move_name} (Type: {self.elemental_type}): {self.low_attack_points} to {self.high_attack_points} Attack Points")
     # randomly generates number between low and high attack points
     def generate_attack_value(self):
-        attack = random.randrange(self.low_attack_points,self.high_attack_points)
+        attack = random.randint(self.low_attack_points,self.high_attack_points)
         return attack
 
 
 class Pokemon:
+    # Constructor - takes name, elemental type, and starting hit points
     def __init__(self, name, elemental_type, hit_points):
         self.name = name
         self.elemental_type = elemental_type
@@ -33,7 +35,7 @@ class Pokemon:
         print(f"{self.name} has been healed to {self.hit_points} hit points.")
 
 lstMoves = []
-
+# Create 9 Move objects with name, elemental type, and attack point range
 Move1 = Move("Tackle", "Normal", 5, 20)
 Move2 = Move("Quick Attack", "Normal", 6, 25)
 Move3 = Move("Slash", "Normal", 10, 30)
@@ -54,11 +56,12 @@ lstMoves.append(Move7)
 lstMoves.append(Move8)
 lstMoves.append(Move9)
 
+# Randomly select 3 moves, print their info and attack value, then remove them from the list
 for iCount in range(0,3): 
     move_index = random.randrange(len(lstMoves))
     move = lstMoves[move_index]
 
-    move.get_info()
+    print(move.get_info())
     print(f"Generated attack value: {move.generate_attack_value()}")
     
 
